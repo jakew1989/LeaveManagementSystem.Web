ec5416237bc25c6c2503ef28671f15cec0694742
Jake Wright <jake.wright99@yahoo.com>
2024-07-13 18:43:20 +0100

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DOB = new DateOnly(1989,12,17)
+            };
+            
+            return View(data);
+        }
+
+
+    }
+}
---
+++ Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateOnly? DOB { get; set; }
+    }
+}
